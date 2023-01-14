df7309a1e14ad554c048e8d6bfdc1763977e4580
Feyyaz Beğen <feyyazbegen61@gmail.com>
2023-01-14 23:00:59 +0300

    CurrencyExchange servisi oluşturuldu

---
+++ currency-exchange-service/src/main/java/com/feyyazbegen/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,19 @@
+package com.feyyazbegen.microservices.currencyexchangeservice;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrencyExchange {
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+}
