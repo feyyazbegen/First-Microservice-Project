a923f78b419a23b27d1d734f9d89820d25bafe10
Feyyaz Beğen <feyyazbegen61@gmail.com>
2023-01-15 18:38:34 +0300

    Mikroservisler birbirine bağlandı

--- currency-exchange-service/src/main/java/com/feyyazbegen/microservices/currencyexchangeservice/CurrencyExchangeRepository.java
+++ currency-exchange-service/src/main/java/com/feyyazbegen/microservices/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange,Long> {
 
-    CurrencyExchange findByFromAndTo(String from,String to;
+    CurrencyExchange findByFromAndTo(String from,String to);
 }
